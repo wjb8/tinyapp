d1674988cac971c767c3440d74fa94065ecee4c9
James Brown <brownj95@gmail.com>
2021-04-14 20:53:50 +0000

    implemented registration/login features

--- express_server.js
+++ express_server.js
@@ -15,6 +15,39 @@ const generateRandomString = (length) => {
   return result.join('');
 };
 
+const formsAreEmpty = (req) => {
+  if (!req.body.email || !req.body.password) {
+    return true;
+  }
+  return false;
+};
+
+// const emailAlreadyExists = (users, req) => {
+//   for (let user in users) {
+//     console.log(users[user].email);
+//     // console.log(req.body.email);
+//     if (users[user].email === req.body.email) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+const getUserByEmail = (users, req) => {
+  for (let user in users) {
+    if (users[user].email === req.body.email) {
+      return users[user];
+    }
+  }
+};
+
+const checkPassword = (users, req) => {
+  if (getUserByEmail(users, req).password === req.body.password) {
+    return true;
+  }
+  return false;
+};
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
@@ -25,22 +58,84 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 app.get('/', (req, res) => {
   res.send('Hello! Welcome to TinyApp!');
 });
 
+app.get('/login', (req, res) => {
+  const user = req.cookies['user_id'];
+  const templateVars = {user: users[user]};
+  res.render('login', templateVars);
+});
+
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
+  
+  if (formsAreEmpty(req)) {
+    res.statusCode = 400;
+    res.send('Error: forms are empty');
+  }
+
+  if (!getUserByEmail(users, req)) {
+    res.statusCode = 403;
+    res.send('Error: User does not exist');
+    return;
+  }
+
+  if (!checkPassword(users, req)) {
+    res.statusCode = 403;
+    res.send('Error: Incorrect password');
+    return;
+  }
+
+  res.cookie('user_id', getUserByEmail(users, req).id);
   res.redirect('/urls');
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
-})
+});
+
+app.get('/register', (req, res) => {
+  const user = req.cookies['user_id'];
+  const templateVars = {user: users[user]};
+  res.render('register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  
+  if (formsAreEmpty(req)) {
+    res.statusCode = 400;
+    res.send('Error: forms are empty');
+  }
+
+  if (getUserByEmail(users, req)) {
+    res.statusCode = 403;
+    res.send('Error: User already exists');
+  }
+
+  const id = generateRandomString(6);
+  users[id] = {id: id, email: req.body.email, password: req.body.password};
+  res.cookie('user_id', getUserByEmail(users, req).id);
+  res.redirect('/urls');
+});
 
 app.get('/urls', (req, res) => {
-  const templateVars = {username: req.cookies["username"], urls: urlDatabase};
+  const user = req.cookies['user_id'];
+  const templateVars = {user: users[user], urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
@@ -51,8 +146,9 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {username: req.cookies["username"]};
-  res.render("urls_new");
+  const user = req.cookies['user_id'];
+  const templateVars = {user: users[user]};
+  res.render("urls_new", templateVars);
 });
 
 app.get('/u/:shortURL', (req, res) => {
@@ -61,7 +157,8 @@ app.get('/u/:shortURL', (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const user = req.cookies['user_id'];
+  const templateVars = {user: users[user], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
@@ -86,5 +183,5 @@ app.get("/hello", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`TinyApp listening on port ${PORT}`);
 });
